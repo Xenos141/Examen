ffd6f30ebd26bd91c94153bba770a74a0eaaca9e
Xenos141 <arturomr2001@hotmail.com>
2018-10-17 08:45:36 -0500

    Agregamos imagen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { CancionesPage } from '../pages/canciones/canciones';
+import { ImagenPage } from '../pages/imagen/imagen';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    CancionesPage,
+    ImagenPage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +23,9 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    CancionesPage,
+    ImagenPage
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,24 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { CancionesPage } from '../canciones/canciones';
+import { ImagenPage } from '../imagen/imagen';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+Cancion = CancionesPage
+Imagen = ImagenPage
   constructor(public navCtrl: NavController) {
 
   }
-
+Canciones1()
+{
+  this.navCtrl.push(this.Cancion)
+}
+Imagen1()
+{
+  this.navCtrl.push(this.Imagen)
+}
 }
